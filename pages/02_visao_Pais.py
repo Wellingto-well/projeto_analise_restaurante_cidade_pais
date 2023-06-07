b955bbc0ecc1375fd25e2d3f08d481e328a115ff
Wellington <113539794+Wellington-well@users.noreply.github.com>
2023-06-06 23:03:07 -0300

    Update 02_visao_Pais.py

--- pages/02_visao_Pais.py
+++ pages/02_visao_Pais.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px 
 
-df = pd.read_csv('pages\\zomato.csv')
+df = pd.read_csv('zomato.csv')
 
 st.set_page_config(page_title="Países", page_icon="🌍", layout="wide")
 
